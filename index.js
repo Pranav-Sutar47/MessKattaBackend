470e99872785d047861ffb06bc0ecfecbb622394
Pranav Sutar <160314020+Pranav-Sutar47@users.noreply.github.com>
2024-12-26 15:28:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use(cors({
-    origin:'https://messkatta.netlify.app/',
+    origin:'https://messkatta.netlify.app',
     methods:'GET,POST,PUT,DELETE',
     credentials:true,
 }));
